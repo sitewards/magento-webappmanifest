86adca7e68b60b46b30d182dbd642e34c6448d93
Andrew Howden <hello@andrewhowden.com>
2017-06-27 16:52:56 +0200

    AD-HOC feat (*): Create initial Magento files
    The magento composer installer will fail to install any Magento
    extension that is not provided in the expected format. This includes:
    
    1. A modman file
    2. Some minimal Magento configuration
    
    This commit adds a minimal set of configuration files to declare a
    Magento extension, but does not implement any feature in them.

---
+++ src/app/code/community/Sitewards/Webappmanifest/Helper/Data.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Web application manifest implementation for Magento 1.x
+ *
+ * @category  Sitewards
+ * @package   Sitewards_Webappmanifest
+ * @copyright Copyright (c) Sitewards GmbH (http://www.sitewards.com)
+ * @contact   mail@sitewards.com
+ */
+
+/**
+ * This is a concrete implementation of the abstract helper class, so that the system configuration can access the
+ * translation function (__)
+ */
+class Sitewards_Webappmanifest_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+
